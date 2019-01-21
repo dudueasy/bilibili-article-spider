56b622347991233832df9db9a9e30c578c41aeb2
dudueasy <dudueasy@126.com>
2019-01-21 18:29:29 +0800

    new feature: 在生成 redis 资源 id 的时候将会生成一条记录到 id_generation_record.txt 文件中

--- scripts/spider.js
+++ scripts/spider.js
@@ -3,6 +3,7 @@
 // example1: node spider generate_ids 1 12
 // example2: node spider start_getting_articles
 
+const fs = require('fs')
 const path = require('path')
 const RedisService = require('../services/content_id_service');
 const spiderService = require('../services/spider_service');
@@ -16,8 +17,12 @@ switch (defaultTask) {
     let startpoint = Number(process.argv[3]);
     let endpoint = Number(process.argv[4]);
     RedisService.generateResourceIdToRedis(startpoint, endpoint)
-      .then((success) => {
-          console.log(`ids from ${startpoint}0000 to ${endpoint}0000 were added to local redis database!` );
+      .then((success) => { 
+          createAt = Date(Date.now()).toString()
+          let idGenerationRecord = `ids from ${startpoint}0000 to ${endpoint}0000 were added to local redis database at ${createAt}!`
+          fs.writeFileSync('id_generation_record.txt',`\n${idGenerationRecord}`,{encoding:'utf-8',flag:'a'})
+          console.log(idGenerationRecord)
+          process.exit(0); 
           process.exit(0);
         },
       )
