04e71be0122a590e9c8ef82dac90abfe14b5cf31
dudueasy <dudueasy@126.com>
2019-04-01 00:26:47 +0800

    将连接 mongodb 数据库的语句添加到 model 中

--- models/article.js
+++ models/article.js
@@ -1,16 +1,33 @@
-// this module returns a mongoose Model for bilibili articles storage
-const mongoose =  require('mongoose')
-const { Schema } = mongoose
+// this module build mongoose db connection.
+// call this module before using mongoose model api
+const path = require('path');
+const mongoose = require('mongoose');
+let logger = require('../utils/loggers/logger');
 
-const ArticleSchema = new Schema({
-  resourceId: String,
-  content: Schema.Types.Mixed,
-  articleContentHtml: String,
-  createdAt: { type: String, default: Date.now().valueOf() },
-  originalCreatedAt:String,
-  title: {type: String, required: true},
-  tags: [{name: String, value: String, score: Number}],
-})
+require('dotenv').config({path: path.join(__dirname, '.env')});
 
-const articleModel = mongoose.model('article',ArticleSchema)
-module.exports = articleModel
+mongoose.set('useFindAndModify', false);
+mongoose.Promise = Promise;
+
+
+DB_RESOURCE_DB = process.env.DB_RESOURCE_DB;
+DB_URL = process.env.DB_URL;
+
+const uri = `${DB_URL}/${DB_RESOURCE_DB}`;
+
+
+mongoose.connect(uri, {useNewUrlParser: true});
+let db = mongoose.connection;
+
+db.on('open', () => {
+  logger('info', 'info', `mongodb connection built through mongoose, connected database: ${process.env.DB_RESOURCE_DB}`);
+});
+
+db.on('error', (e) => {
+  logger('error', 'Mongodb connection error: %s', e.message, {stack: e.stack});
+  process.exit(1);
+});
+
+// mongoodb 连接测试代码
+
+module.exports = db;
